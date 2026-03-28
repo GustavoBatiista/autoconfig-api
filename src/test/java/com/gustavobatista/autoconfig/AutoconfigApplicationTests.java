19e52a0c634b67f3daec22c04704584402dd7b13
GustavoBatiista <gustavobatista18@hotmail.com>
2026-03-27 23:05:33 -0300

    fix(ci): remove dev profile override and fix jwt for test context

--- src/test/java/com/gustavobatista/autoconfig/AutoconfigApplicationTests.java
+++ src/test/java/com/gustavobatista/autoconfig/AutoconfigApplicationTests.java
@@ -2,8 +2,10 @@ package com.gustavobatista.autoconfig;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AutoconfigApplicationTests {
 
 	@Test
