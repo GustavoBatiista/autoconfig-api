cf3e8b1ee33d1dccf27b161bfebca50d4b2eebb0
GustavoBatiista <gustavobatista18@hotmail.com>
2026-04-14 22:10:27 -0300

    refactor(frontend): register routes for edit and delete pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,6 +6,8 @@ import { SellerLayout } from './layouts/SellerLayout'
 import { ManagerLayout } from './layouts/ManagerLayout'
 import { OrdersPage } from './pages/orders/OrdersPage'
 import { OrderCreatePage } from './pages/orders/OrderCreatePage'
+import { OrderEditPage } from './pages/orders/OrderEditPage'
+import { OrderDeletePage } from './pages/orders/OrderDeletePage'
 import { CarsPage } from './pages/cars/CarsPage'
 import { ClientsPage } from './pages/clients/ClientsPage'
 import { AccessoriesPage } from './pages/accessories/AccessoriesPage'
@@ -13,8 +15,14 @@ import { VehicleStockPage } from './pages/vehicles/VehicleStockPage'
 import { VehicleEntryCreatePage } from './pages/vehicles/VehicleEntryCreatePage'
 import { UsersPage } from './pages/users/UsersPage'
 import { ClientCreatePage } from './pages/clients/ClientCreatePage'
+import { ClientEditPage } from './pages/clients/ClientEditPage'
+import { ClientDeletePage } from './pages/clients/ClientDeletePage'
 import { CarCreatePage } from './pages/cars/CarCreatePage'
+import { CarEditPage } from './pages/cars/CarEditPage'
+import { CarDeletePage } from './pages/cars/CarDeletePage'
 import { AccessoryCreatePage } from './pages/accessories/AccessoryCreatePage'
+import { AccessoryEditPage } from './pages/accessories/AccessoryEditPage'
+import { AccessoryDeletePage } from './pages/accessories/AccessoryDeletePage'
 import { getAccessToken } from './api/tokenStorage'
 
 function ProtectedLayout() {
@@ -33,19 +41,31 @@ export default function App() {
         <Route path="seller" element={<SellerLayout />}>
           <Route index element={<OrdersPage />} />
           <Route path="orders/new" element={<OrderCreatePage />} />
+          <Route path="orders/edit" element={<OrderEditPage />} />
+          <Route path="orders/delete" element={<OrderDeletePage />} />
           <Route path="cars" element={<CarsPage />} />
           <Route path="clients" element={<ClientsPage />} />
           <Route path="clients/new" element={<ClientCreatePage />} />
+          <Route path="clients/edit" element={<ClientEditPage />} />
+          <Route path="clients/delete" element={<ClientDeletePage />} />
         </Route>
         <Route path="manager" element={<ManagerLayout />}>
           <Route index element={<OrdersPage />} />
           <Route path="orders/new" element={<OrderCreatePage />} />
+          <Route path="orders/edit" element={<OrderEditPage />} />
+          <Route path="orders/delete" element={<OrderDeletePage />} />
           <Route path="cars" element={<CarsPage />} />
           <Route path="cars/new" element={<CarCreatePage />} />
+          <Route path="cars/edit" element={<CarEditPage />} />
+          <Route path="cars/delete" element={<CarDeletePage />} />
           <Route path="clients" element={<ClientsPage />} />
           <Route path="clients/new" element={<ClientCreatePage />} />
+          <Route path="clients/edit" element={<ClientEditPage />} />
+          <Route path="clients/delete" element={<ClientDeletePage />} />
           <Route path="accessories" element={<AccessoriesPage />} />
           <Route path="accessories/new" element={<AccessoryCreatePage />} />
+          <Route path="accessories/edit" element={<AccessoryEditPage />} />
+          <Route path="accessories/delete" element={<AccessoryDeletePage />} />
           <Route path="vehicles" element={<VehicleStockPage />} />
           <Route path="vehicles/new" element={<VehicleEntryCreatePage />} />
           <Route path="users" element={<UsersPage />} />
