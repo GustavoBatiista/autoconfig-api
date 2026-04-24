01619b9939ece607a0ec4f2b9d4d6941ab040057
GustavoBatiista <gustavobatista18@hotmail.com>
2026-04-24 00:14:31 -0300

    fix(db): make V5 migration safe for new environments by removing rename and using drop-if-exists strategy

--- src/main/resources/db/migration/V5__order_accessory_snapshot.sql
+++ src/main/resources/db/migration/V5__order_accessory_snapshot.sql
@@ -1,4 +1,5 @@
-RENAME TABLE order_accessory TO order_accessory_old;
+DROP TABLE IF EXISTS order_accessory_old;
+DROP TABLE IF EXISTS order_accessory;
 
 CREATE TABLE order_accessory (
     id BIGINT NOT NULL AUTO_INCREMENT,
@@ -14,18 +15,3 @@ CREATE TABLE order_accessory (
     CONSTRAINT fk_order_accessory_order FOREIGN KEY (order_id) REFERENCES orders (id) ON DELETE CASCADE,
     CONSTRAINT fk_order_accessory_accessory FOREIGN KEY (accessory_id) REFERENCES accessory (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-INSERT INTO order_accessory (order_id, accessory_id, name, price, created_at, updated_at, created_by, updated_by)
-SELECT
-    oa.order_id,
-    oa.accessory_id,
-    a.name,
-    a.price,
-    a.created_at,
-    a.updated_at,
-    a.created_by,
-    a.updated_by
-FROM order_accessory_old oa
-INNER JOIN accessory a ON a.id = oa.accessory_id;
-
-DROP TABLE order_accessory_old;
